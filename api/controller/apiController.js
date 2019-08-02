97365269618389872602ce1776047586b2ce7cf0
GRomaric <noksit@live.fr>
2019-08-02 14:29:11 +0200

    mise en place de ffmpeg-static module required par youtubemp3downloader module

--- api/controller/apiController.js
+++ api/controller/apiController.js
@@ -1,8 +1,11 @@
 'use strict';
 let YoutubeMp3Downloader = require('youtube-mp3-downloader');
+let ffmpeg = require('ffmpeg-static');
 
 exports.download = function (req, res) {
+    console.log(ffmpeg.path);
     let YD = new YoutubeMp3Downloader({
+        "ffmpeg": ffmpeg.path,
         "outputPath": "output",    // Where should the downloaded and encoded files be stored?
         "youtubeVideoQuality": "highest",       // What video quality should be used?
     });
