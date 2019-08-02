4b10f5a98e589a159bbed16dea2add4d6dd8fff0
GRomaric <noksit@live.fr>
2019-08-02 15:08:29 +0200

    ajout d'un bouton, gestion de l'input avec le key Enter. refacto du show / hide, ajout des log plus propre en English

--- api/controller/apiController.js
+++ api/controller/apiController.js
@@ -3,23 +3,23 @@ let YoutubeMp3Downloader = require('youtube-mp3-downloader');
 let ffmpeg = require('ffmpeg-static');
 
 exports.download = function (req, res) {
-    console.log(ffmpeg.path);
     let YD = new YoutubeMp3Downloader({
         "ffmpeg": ffmpeg.path,
         "outputPath": "./output",    // Where should the downloaded and encoded files be stored?
         "youtubeVideoQuality": "highest",       // What video quality should be used?
     });
     let id = req.params.id;
-    console.log("Requete recue pour :: " + id);
+    console.log("REQUEST :: " + id);
     YD.download(id);
     YD.on("finished", function (err, data) {
         if (err) {
+            console.log("ERROR ::");
             console.log(err);
             res.json({
                 Erreur: err
             })
         }
-        console.log("Terminée");
+        console.log("DONE");
         res.json({
             ID: data.videoId,
             Nom: data.videoTitle
