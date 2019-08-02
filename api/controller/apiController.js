c39ddfc7afac29102ccda0a36360c2c86e9fcf29
GRomaric <noksit@live.fr>
2019-08-02 15:25:41 +0200

    btn valider en Gotcha // Youtube Urbl en Downloader::

--- api/controller/apiController.js
+++ api/controller/apiController.js
@@ -5,7 +5,7 @@ let ffmpeg = require('ffmpeg-static');
 exports.download = function (req, res) {
     let YD = new YoutubeMp3Downloader({
         "ffmpeg": ffmpeg.path,
-        "outputPath": "./output",    // Where should the downloaded and encoded files be stored?
+        "outputPath": "./output",               // Where should the downloaded and encoded files be stored?
         "youtubeVideoQuality": "highest",       // What video quality should be used?
     });
     let id = req.params.id;
